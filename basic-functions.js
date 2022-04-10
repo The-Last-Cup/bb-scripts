8c498bb18520828cb9b5702668a5415bf6b1aef3
The-Last-Cup <96949522+The-Last-Cup@users.noreply.github.com>
2022-04-10 15:55:50 +0800

    1

--- basic-functions.js
+++ basic-functions.js
@@ -1,1 +1,17 @@
-export function 
+//return player's current money
+export function myMoney(ns) {
+	var money = ns.getServerMoneyAvailable("home");
+	return money;
+}
+//check if player own an sf of certain lvl
+export function ownSF(ns,n,lvl) {
+	var name = n;
+	var level = lvl;
+	var ownedSF = ns.getOwnedSourceFiles();
+	for (let i of ownedSF) {
+		if (i.n == name && i.lvl >= level ) {
+			return true;
+		}
+	}
+	return false;
+}
